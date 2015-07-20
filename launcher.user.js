a5da8fc5f335e52bf23ee268d561fc6413a178d2
furkangoktasf <furkan-goktasf@hotmail.com>
2015-07-21 01:32:31 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposLauncher
 // @namespace   AposLauncher
-// @include     http://agar.io/*
+// @include     http://beta.agariopvp.com/*
 // @version     3.052
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
